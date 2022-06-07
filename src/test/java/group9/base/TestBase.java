49c8cc5debbcdcf29a095327a3100422b84d3121
petro <petrokozovyi@gmail.com>
2022-06-07 16:01:28 -0500

    DropdownSelectingDate task

---
+++ src/test/java/group9/base/TestBase.java
@@ -0,0 +1,28 @@
+package group9.base;
+
+import group9.petro.utilities.WebDriverFactory;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+import java.util.concurrent.TimeUnit;
+
+public class TestBase {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod() {
+        //1. Open browser
+        driver = WebDriverFactory.getDriver("chrome");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+    }
+
+    @AfterMethod
+    public void tearDownMethod() {
+        driver.close();
+    }
+}
+
+
---
+++ src/test/java/group9/petro/homework/DropdownSelectingDate.java
@@ -0,0 +1,37 @@
+package group9.petro.homework;
+
+import group9.base.TestBase;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+//1. Open Chrome browser
+public class DropdownSelectingDate extends TestBase {
+
+    @Test
+    public void dropdownSelectingDate() {
+
+        //2. Go to https://practice.cydeo.com/dropdown
+        driver.get("https://practice.cydeo.com/dropdown");
+
+        // Select year using   : visible text
+        Select year = new Select(driver.findElement(By.id("year")));
+        year.selectByVisibleText("1923");
+
+        //Select month using    : value attribute Select
+        Select month = new Select(driver.findElement(By.id("month")));
+        month.selectByValue("11");
+
+        //day using : index number
+        Select day = new Select(driver.findElement(By.id("day")));
+        day.selectByIndex(0);
+
+        //3. Select “December 1st, 1923” and verify it is selected.
+        Assert.assertEquals(year.getFirstSelectedOption().getText(), "1923","Wrong input");
+        Assert.assertEquals(month.getFirstSelectedOption().getText(),"December","Wrong input");
+        Assert.assertEquals(day.getFirstSelectedOption().getText(),"1","Wrong input");
+
+    }
+}
